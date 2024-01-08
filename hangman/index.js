f47f95ef4b130e8763dabc27d066da701fa02c63
Irina Fileva <irinkaskai@gmail.com>
2024-01-08 20:01:12 +0500

    feat: add images gallows and loop

--- hangman/index.js
+++ hangman/index.js
@@ -11,8 +11,20 @@ addElement('div', 'game__wrapper', body);
 
 const gameWrapper = document.querySelector('.game__wrapper');
 
-addElement('h1', 'game__title', gameWrapper)
+addElement('h1', 'game__title', gameWrapper);
+addElement('img', 'game__img-gallows', gameWrapper);
+addElement('img', 'game__img-loop', gameWrapper)
 
 const gameName= document.querySelector('.game__title');
+const imgGallows = document.querySelector('.game__img-gallows');
+const imgLoop = document.querySelector('.game__img-loop');
 
-gameName.innerText = 'HANGMAN'
+gameName.innerText = 'HANGMAN';
+
+imgGallows.src = 'assets/gallows.png';
+imgGallows.alt = 'Gallows';
+
+imgLoop.src = 'assets/loop.png';
+imgLoop.alt = 'Loop';
+
+
