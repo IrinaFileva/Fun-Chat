5629e56d0ad01e1404760fec7475957697be0552
Irina Fileva <irinkaskai@gmail.com>
2024-02-24 12:55:03 +0500

    feat: add type folder with index.ts file

---
+++ news-api/src/components/type/index.ts
@@ -0,0 +1,9 @@
+interface NewsArticle{
+    author: string;
+    source: { name: string};
+    publishedAt: string;
+    title: string;
+    description: string;
+    url: string;
+    urlToImage: string;
+}
