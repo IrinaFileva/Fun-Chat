6f0ab20df479416850ba76850c548fe9d8dd1a93
Irina Fileva <irinkaskai@gmail.com>
2024-04-08 15:35:46 +0500

    init: start task fun-chat

---
+++ fun-chat/webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const EslintPlugin = require('eslint-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { merge } = require('webpack-merge');
+const webpackProd = require('./webpack.prod.config');
+const webpackDev = require('./webpack.dev.config');
+
+const baseConfig = {
+  entry: path.resolve(__dirname, './src/index'),
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.(html)$/i,
+        use: ['html-loader'],
+      },
+      {
+        test: /\.ts$/i,
+        use: 'ts-loader',
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.json'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ title: 'Fun Chat' }),
+    new CleanWebpackPlugin(),
+    new EslintPlugin({ extensions: 'ts' }),
+  ],
+};
+
+module.exports = ({ mode }) => {
+  const isProductionMode = mode === 'prod';
+  const envConfig = isProductionMode ? webpackProd : webpackDev;
+
+  return merge(baseConfig, envConfig);
+};
