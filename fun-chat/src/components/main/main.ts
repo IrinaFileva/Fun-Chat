d6e4af174a8bc24452ddd57dc3f8c761bab7f343
Irina Fileva <irinkaskai@gmail.com>
2024-04-15 22:41:48 +0500

    feat: add user search function via input search and a field to show messages

--- fun-chat/src/components/main/main.ts
+++ fun-chat/src/components/main/main.ts
@@ -1,3 +1,4 @@
+import { serverRequests } from '../../api/serverRequests ';
 import { InputForm } from '../forms/componentsForm';
 import './styleMain.css';
 
@@ -6,19 +7,42 @@ export class Main {
 
   wrapperList: HTMLElement;
 
+  wrapperMessage: HTMLElement;
+
   constructor() {
     this.item = document.createElement('main');
     this.item.className = 'main-page-main';
     this.wrapperList = document.createElement('div');
     this.wrapperList.className = 'wrapper-user-list';
+    this.wrapperMessage = document.createElement('div');
+    this.wrapperMessage.className = 'wrapper-message-input';
     this.FillWrapperList();
-    this.item.append(this.wrapperList);
+    this.item.append(this.wrapperList, this.wrapperMessage);
   }
 
   private FillWrapperList() {
     const list: HTMLUListElement = document.createElement('ul');
     list.className = 'list-users';
-    const input = new InputForm('input-search', 'search').item;
+    const input: HTMLInputElement = this.addInputSearch();
     this.wrapperList.append(input, list);
+  }
+
+  private addInputSearch(): HTMLInputElement {
+    const input: HTMLInputElement = new InputForm('input-search', 'search').item;
+    input.addEventListener('input', () => {
+      const allUser: NodeListOf<HTMLElement> = document.querySelectorAll('.item-list-name-user');
+      allUser.forEach((elem: HTMLElement) => {
+        const userName: string | null = elem.textContent;
+        const parent: HTMLElement | null = elem.parentElement;
+        if (parent) {
+          if (userName && !userName.includes(input.value)) {
+            parent.style.display = 'none';
+          } else {
+            parent.style.display = 'list-item';
+          }
+        }
+      });
+    });
+    return input;
   }
 }
